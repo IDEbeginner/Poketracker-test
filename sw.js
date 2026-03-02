b9bfe77d8c0146246a5a2b7315c884a0390ebe5d
IDEbeginner <vidalneilpro@gmail.com>
2026-03-02 15:18:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C=“poketracker-v2”;
+self.addEventListener(“install”,e=>{e.waitUntil(caches.open(C).then(c=>c.addAll([”./”,”./index.html”,”./logo.png”])));self.skipWaiting()});
+self.addEventListener(“activate”,e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener(“fetch”,e=>{const u=new URL(e.request.url);if([“firebaseio”,“googleapis”,“firestore”,“identitytoolkit”,“securetoken”,“tcgdex”,“pokecardex”].some(h=>u.hostname.includes(h)))return;if([“raw.githubusercontent.com”,“assets.tcgdex.net”,“pokesymbols.com”].some(h=>u.hostname.includes(h))){e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok)caches.open(C).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>new Response(””,{status:404}))));return}e.respondWith(fetch(e.request).then(r=>{if(r.ok)caches.open(C).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)))});
